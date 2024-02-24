ba46a877d1eb47eaf840f5d5f388937669c890e1
Skyler James <15920401+skyjay1@users.noreply.github.com>
2024-02-24 11:06:38 -0700

    refresh cached recipes when tags are updated

--- src/main/java/tanukidecor/client/ClientRecipeCollections.java
+++ src/main/java/tanukidecor/client/ClientRecipeCollections.java
@@ -14,9 +14,9 @@ import net.minecraft.client.searchtree.MutableSearchTree;
 import net.minecraft.client.searchtree.ReloadableSearchTree;
 import net.minecraft.client.searchtree.SearchRegistry;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraftforge.client.event.RecipesUpdatedEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TagsUpdatedEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -35,16 +35,16 @@ public final class ClientRecipeCollections {
     public static void register() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(ClientRecipeCollections::onClientSetup);
         MinecraftForge.EVENT_BUS.addListener(ClientRecipeCollections::onUpdateRecipes);
+        MinecraftForge.EVENT_BUS.addListener(ClientRecipeCollections::onUpdateTags);
     }
 
     private static void onClientSetup(FMLClientSetupEvent event) {
         event.enqueueWork(ClientRecipeCollections::registerSearchTrees);
     }
 
-    private static void onUpdateRecipes(final RecipesUpdatedEvent event) {
-        ClientRecipeCollections.updateRecipesAndSearchTree(event.getRecipeManager());
-    }
-
+    /**
+     * Registers a search tree of {@link RecipeCollection}s that is indexed by item names
+     **/
     private static void registerSearchTrees() {
         // create a search tree (copied from Minecraft#createSearchTrees)
         final ReloadableSearchTree<RecipeCollection> searchTree = new ReloadableSearchTree<>((recipes) ->
@@ -59,14 +59,19 @@ public final class ClientRecipeCollections {
         Minecraft.getInstance().getSearchTreeManager().register(ClientRecipeCollections.DIY_RECIPE_COLLECTIONS_KEY, searchTree);
     }
 
-    private static void updateRecipesAndSearchTree(final RecipeManager recipeManager) {
+    /**
+     * Populates the recipe cache and search tree with {@link RecipeCollection}s that contain
+     * one {@link tanukidecor.recipe.DIYRecipe} each
+     * @param event the recipe update event
+     */
+    private static void onUpdateRecipes(final RecipesUpdatedEvent event) {
         // load search tree
         MutableSearchTree<RecipeCollection> searchTree = Minecraft.getInstance().getSearchTree(DIY_RECIPE_COLLECTIONS_KEY);
         // clear old recipes
         DIY_RECIPE_COLLECTIONS.clear();
         searchTree.clear();
         // add updated recipes to search tree, one recipe per RecipeCollection
-        recipeManager
+        event.getRecipeManager()
                 .getAllRecipesFor(TDRegistry.RecipeReg.DIY.get())
                 .stream()
                 .map(recipe -> new RecipeCollection(ImmutableList.of(recipe)))
@@ -74,6 +79,28 @@ public final class ClientRecipeCollections {
                     DIY_RECIPE_COLLECTIONS.add(recipeCollection);
                     searchTree.add(recipeCollection);
                 });
+        // refresh search tree
+        searchTree.refresh();
+    }
+
+    /**
+     * Filters the recipe cache and search tree to remove any blacklisted items
+     * @param event the tag update event
+     */
+    private static void onUpdateTags(final TagsUpdatedEvent event) {
+        // load search tree
+        MutableSearchTree<RecipeCollection> searchTree = Minecraft.getInstance().getSearchTree(DIY_RECIPE_COLLECTIONS_KEY);
+        // clear old recipes
+        final List<RecipeCollection> recipes = ImmutableList.copyOf(DIY_RECIPE_COLLECTIONS);
+        DIY_RECIPE_COLLECTIONS.clear();
+        searchTree.clear();
+        // add all recipes that are not blacklisted back into the list and search tree
+        for(RecipeCollection recipeCollection : recipes) {
+            if(recipeCollection.getRecipes().size() == 1 && !recipeCollection.getRecipes().get(0).getResultItem().is(TDRegistry.DIY_BLACKLIST_TAG_KEY)) {
+                DIY_RECIPE_COLLECTIONS.add(recipeCollection);
+                searchTree.add(recipeCollection);
+            }
+        }
         // refresh search tree
         searchTree.refresh();
     }
--- src/main/java/tanukidecor/client/menu/DIYWorkbenchScreen.java
+++ src/main/java/tanukidecor/client/menu/DIYWorkbenchScreen.java
@@ -70,14 +70,8 @@ public class DIYWorkbenchScreen extends AbstractContainerScreen<DIYWorkbenchMenu
 
     public DIYWorkbenchScreen(DIYWorkbenchMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
-        // collect recipes, excluding any that result in a blacklisted item
-        final ImmutableList.Builder<RecipeCollection> recipesBuilder = ImmutableList.builder();
-        for(RecipeCollection recipeCollection : ClientRecipeCollections.DIY_RECIPE_COLLECTIONS) {
-            if(recipeCollection.getRecipes().size() == 1 && !recipeCollection.getRecipes().get(0).getResultItem().is(TDRegistry.DIY_BLACKLIST_TAG_KEY)) {
-                recipesBuilder.add(recipeCollection);
-            }
-        }
-        this.recipes = ImmutableList.sortedCopyOf(SORT_RECIPES_BY_RESULT_NAME, recipesBuilder.build());
+        // populate recipe collection
+        this.recipes = ImmutableList.sortedCopyOf(SORT_RECIPES_BY_RESULT_NAME, ClientRecipeCollections.DIY_RECIPE_COLLECTIONS);
         // prepare search tree and recipe slice
         this.searchTree = Minecraft.getInstance().getSearchTree(ClientRecipeCollections.DIY_RECIPE_COLLECTIONS_KEY);
         this.sortedRecipes = new ArrayList<>(this.recipes.size());
