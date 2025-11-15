58f0419de303ada1b43000b776ee666f6df3520e
Alex Lee <islicy@outlook.com>
2025-11-15 11:27:06 +0900

    Refactor GlobeBlock interaction method to support item usage, enhancing player interaction capabilities.

--- src/main/java/tanukidecor/block/misc/GlobeBlock.java
+++ src/main/java/tanukidecor/block/misc/GlobeBlock.java
@@ -7,8 +7,11 @@
 package tanukidecor.block.misc;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
@@ -45,8 +48,9 @@ public class GlobeBlock extends RotatingBlock implements EntityBlock {
     /// / METHODS ////
 
     @Override
-    protected InteractionResult useWithoutItem(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, BlockHitResult pHitResult) {
-        return GlobeBlockEntity.use(pState, pLevel, pPos, pPlayer, pHitResult);
+    protected ItemInteractionResult useItemOn(ItemStack pStack, BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHitResult) {
+        InteractionResult result = GlobeBlockEntity.use(pState, pLevel, pPos, pPlayer, pHand, pHitResult);
+        return result == InteractionResult.SUCCESS ? ItemInteractionResult.SUCCESS : ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
     }
 
     /// / BLOCK ENTITY ////
