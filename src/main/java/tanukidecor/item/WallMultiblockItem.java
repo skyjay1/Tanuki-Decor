fb88f567a9f231e4d70d7beb90c1cdeba84f6f43
Skyler James <15920401+skyjay1@users.noreply.github.com>
2026-01-09 19:10:49 -0700

    Update copyright

--- src/main/java/tanukidecor/item/WallMultiblockItem.java
+++ src/main/java/tanukidecor/item/WallMultiblockItem.java
@@ -1,8 +1,8 @@
-/**
- * Copyright (c) 2023 Skyler James
+/*
+ * Copyright (c) 2026 Skyler James
  * Permission is granted to use, modify, and redistribute this software, in parts or in whole,
  * under the GNU LGPLv3 license (https://www.gnu.org/licenses/lgpl-3.0.en.html)
- **/
+ */
 
 package tanukidecor.item;
 
